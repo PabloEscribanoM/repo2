7edeaf9625b6ac80395933f5d0d2c66748528c4b
Jose Manuel Gordon Alonso <jogordon@ucm.es>
2023-05-01 17:10:07 +0200

    tralara

--- src/main/java/viewControllers/jabatosrb/pfdamapj/EntrenadorViewController.java
+++ src/main/java/viewControllers/jabatosrb/pfdamapj/EntrenadorViewController.java
@@ -5,13 +5,18 @@ import jabatosrb.pfdampj.PersistentData;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
 import java.text.ParseException;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class EntrenadorViewController extends ViewUtilities implements Initializable {
@@ -62,12 +67,64 @@ public class EntrenadorViewController extends ViewUtilities implements Initializ
         }
     }
 
-    public void actionNext_mod(ActionEvent actionEvent) {
+    public void actionNext_mod(ActionEvent actionEvent) throws SQLException, ParseException {
+        if(!validarCampos().equals("OK")){
+            textErr.setText(validarCampos());//POR HACER
+        }else{
+
+            PersistentData.getEntrenadorMod().setNombre(textNombre.getText().trim());
+            PersistentData.getEntrenadorMod().setApellidos(textApellidos.getText().trim());
+            PersistentData.getEntrenadorMod().setDni(textDni.getText().trim());
+            PersistentData.getEntrenadorMod().setCuentaBancaria(textIBAN.getText().trim());
+            PersistentData.getEntrenadorMod().setSalario(Double.parseDouble(textSalario.getText().trim()));
+            PersistentData.getEntrenadorMod().setCuentaBancaria(textIBAN.getText().trim());
+            PersistentData.getEntrenadorMod().setTelefono(textTelefono.getText().trim());
+            PersistentData.getEntrenadorMod().setCategoria(textCategoria.getText().trim());
+
+            EntrenadoresController.updateEntrenadores(PersistentData.getEntrenadorMod());
+            cerrarVentana(actionEvent);
+        }
     }
 
-    public void actionAdd_del(ActionEvent actionEvent) {
+    public void actionAdd_del(ActionEvent actionEvent) throws SQLException, ParseException, UnsupportedEncodingException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+        if (isMod){
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setHeaderText(null);
+            alert.setTitle("Confirmación de borrado");
+            alert.setContentText("¿Estás seguro de querer borrar a " + PersistentData.getEscuelaMod().getNombre() + "?");
+            alert.showAndWait();
+            if(alert.getResult() == ButtonType.OK){
+                EntrenadoresController.deleteEntrenadores(PersistentData.getEntrenadorMod());
+                cerrarVentana(actionEvent);
+            }
+        }else{
+            if(!validarCampos().equals("OK"))
+                textErr.setText(validarCampos());
+            else{
+                PersistentData.setEntrenadorMod(new Entrenadores(0, textNombre.getText().trim(), textApellidos.getText().trim(), textTelefono.getText().trim(),Double.parseDouble(textSalario.getText().trim()),
+                        new Date(),null,DateFormat.toDate(dateNacimiento.getValue()),textCategoria.getText().trim(),textDni.getText().trim(),
+                        1,textIBAN.getText().trim()));
+
+                EntrenadoresController.addEntrenadores(PersistentData.getEntrenadorMod());
+
+                textNombre.setText("");
+                textApellidos.setText("");
+                textTelefono.setText("");
+                textSalario.setText("");
+                textCategoria.setText("");
+                textIBAN.setText("");
+                textDni.setText("");
+                dateNacimiento.setValue(null);
+            }
+        }
     }
+
 
     public void actionCancel(ActionEvent actionEvent) {
+        cerrarVentana(actionEvent);
+    }
+
+    private String validarCampos() {
+        return "OK";
     }
 }
